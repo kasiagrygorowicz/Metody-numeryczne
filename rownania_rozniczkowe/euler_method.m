85b738cf82275b688aaf12493ef85c10ec51f302
kasiagrygorowicz <kasiagrygorowicz@interia.eu>
2021-05-19 22:19:01 +0200

    eee

--- rownania_rozniczkowe/euler_method.m
+++ rownania_rozniczkowe/euler_method.m
@@ -1,17 +1,17 @@
-function results = euler_method (h, x0, y0, x, f)
-    n = (x-x0)/h;
-    results = zeros(2,n);
-%         rząd 1 na x
-    results(1,1) = x0;
-%         rząd 2 na y
-    results(2,1) = y0;
-    i = 2;
+function y0 = euler_method (h, x0, y0, x, f)
+%     n = (x-x0)/h;
+%     results = zeros(2,n);
+% %         rząd 1 na x
+%     results(1,1) = x0;
+% %         rząd 2 na y
+%     results(2,1) = y0;
+%     i = 2;
     while x0 < x
         y0 = y0 + h*f(x0,y0);
         x0 = x0 + h;
-        results(1,i) = x0;
-        results(2,i) = y0;
-        i = i + 1;
+%         results(1,i) = x0;
+%         results(2,i) = y0;
+%         i = i + 1;
     end
 end
 
--- rownania_rozniczkowe/heun_method.m
+++ rownania_rozniczkowe/heun_method.m
@@ -1,11 +1,11 @@
-function results = heun_method (h, x0, y0, x, f)
-    n = (x-x0)/h;
-    results = zeros(2,n);
-    i = 2;
-%         rząd 1 na x
-        results(1,1) = x0;
-%         rząd 2 na y
-        results(2,1) = y0;
+function y0 = heun_method (h, x0, y0, x, f)
+%     n = (x-x0)/h;
+%     results = zeros(2,n);
+%     i = 2;
+% %         rząd 1 na x
+%         results(1,1) = x0;
+% %         rząd 2 na y
+%         results(2,1) = y0;
     
     while x0 < x
         predicator_y = calculate_predicator(x0,y0,h,f);
@@ -13,8 +13,8 @@ function results = heun_method (h, x0, y0, x, f)
         corrector_y = calculate_corrector(x0,y0,predicator_x,predicator_y,h,f);
         x0 = predicator_x;
         y0 = corrector_y;
-        results(1,i) = x0;
-        results(2,i) = y0;
-        i = i +1;
+%         results(1,i) = x0;
+%         results(2,i) = y0;
+%         i = i +1;
     end
 end
